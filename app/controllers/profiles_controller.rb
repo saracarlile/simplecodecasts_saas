932dc5156c693b092898b6db5521cb401d4e17e1
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-03-28 14:31:04 +0000

    commit changes to profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,11 @@
 class ProfilesController < ApplicationController
-    def new
+  before_action :authenticate_user!
+  before_action :only_current_user
+  
+  def new
     # form where a user can fill out their own profile.
     @user = User.find( params[:user_id] )
-    @variable = params[:hello]
-    @profile = @user.build_profile
-    end
+    @profile = Profile.new
+  end
+  
 end
