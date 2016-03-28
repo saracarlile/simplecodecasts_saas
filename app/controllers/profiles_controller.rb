f769a9236efac33d7e6998e7977f7167cadd8b4b
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-03-28 20:52:14 +0000

    changes to profiles controller and add users controller and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,11 @@
+
 class ProfilesController < ApplicationController
   def new
     # form where a user can fill out their own profile.
     @user = User.find( params[:user_id] )
-    @profile = Profile.new
+    @profile = @user.build_profile
   end
-  
-   def create 
+  def create 
     @user = User.find( params[:user_id] )
     @profile = @user.build_profile(profile_params)
     if @profile.save
@@ -14,8 +14,8 @@ class ProfilesController < ApplicationController
     else
       render action: :new
     end
-    
-    private
+  end
+  private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
     end
