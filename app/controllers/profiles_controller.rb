dcf4601211a07eb15a1798c06911efec19339880
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-04-01 03:02:54 +0000

    Home page improvements

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,7 +3,7 @@ class ProfilesController < ApplicationController
   def new
     # form where a user can fill out their own profile.
     @user = User.find( params[:user_id] )
-    @profile = Profile.new
+    @profile = Profile.newgit 
   end
   def create 
     @user = User.find( params[:user_id] )
